db478e9cbefddeeeef6bbc9e92ce10fae352a663
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-04-22 22:13:59 -0400

    Add files via upload

---
+++ Buscador3.js
@@ -0,0 +1,77 @@
+var Lista3 = [
+  { NAME: "🍿LA CASA DE PAPEL", URL: "https://h5.swplayer.com/es/detail/drama/w4T1wpuJxUu5b0bHfQnU5-Money-Heist-Season-1" },
+  { NAME: "🍿JUEGO DE TRONOS", URL: "https://h5.swplayer.com/es/detail/drama/uf5jenYNRHVO6dKZpo3JY-Game-of-Thrones-Season-1/1" },
+  { NAME: "🍿ONE PIECE", URL: "https://h5.swplayer.com/es/detail/drama/nOFADuVbhbWgsKCJbMGbK-One-Piece%5BRU-Audio%5D" }, 
+  { NAME: "🍿VIS A VIS", URL: "https://h5.swplayer.com/es/detail/drama/br2opIQsBK8hepOpdQEDI-Vis-a-vis-Season-1" },
+  { NAME: "🍿VIKINGOS", URL: "https://h5.swplayer.com/es/detail/drama/3TVD1tXGJbNUvPc0u3mP8-Vikings-Season-1/1" },
+  { NAME: "🍿FAMILY GUY", URL: "https://h5.swplayer.com/es/detail/drama/QTUVOLeIYWjsO8Fv2PcxM-Family-Guy-Season-1" }, 
+  { NAME: "🍿BRICKLEBERRY FULL", URL: "https://h5.swplayer.com/es/detail/drama/tNN9IaUvc8Xqek2jHt2Ek-Brickleberry-Season-1" },
+  { NAME: "🍿LOS SIMPSONS", URL: "https://h5.swplayer.com/es/detail/drama/MNxoJpARCrPHvVPu1ljH1-The-Simpsons-Season-1" }, 
+  { NAME: "🍿OUTLANDER", URL: "https://h5.swplayer.com/es/detail/drama/SC5e4d5u7ZQBPnkhz675G-Outlander-Season-1/1" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }, 
+  { NAME: "🍿", URL: "" },
+  { NAME: "🍿", URL: "" }
+]
+   
+// Selecciona la lista existente
+var lista = document.getElementById("Lista");
+
+// Agrega los nuevos elementos dinámicamente
+Lista3.forEach(item => {
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+
+  a.href = item.URL || item.url;
+  a.textContent = item.NAME || item.name;
+  a.classList.add("Data");
+
+  li.appendChild(a);
+  lista.appendChild(li);
+});
+
+  
+  
+  
+  
+  
+  
