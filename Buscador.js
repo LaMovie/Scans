586b709c66cb03a8c646ff22d68dd48ea35c0b38
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-06-05 09:06:46 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -226,9 +226,9 @@ h1 {
   <li><a href="https://drive.google.com/file/d/1e4zY0WA3N8q8AvaVMEvnbOrPFqMxTK3K/view?usp=drivesdk" class="Data">🍿Scary Movie 5</a></li>
   <li><a href="https://drive.google.com/file/d/1agkAGIXIS569EwtlKID4FIFytU-gJwvP/view?usp=drivesdk" class="Data">🍿La Milla Verde</a></li>
   <li><a href="https://lamovie.github.io/AIDA-FULL/" class="Data">🍿Aida Full</a></li>
-  <li><a href="https://drive.google.com/file/d/1EjO8ZRD-0g9g8s-36tDb-8dCJYjLqNWA/view?usp=drivesdk" class="Data">🍿FRAGMENTADO 2 (GLASS)</a></li> 
+  <li><a href="https://drive.google.com/file/d/1EjO8ZRD-0g9g8s-36tDb-8dCJYjLqNWA/view?usp=drivesdk" class="Data">🍿Fragmentado 2 (GLASS)</a></li> 
   <li><a href="https://drive.google.com/file/d/1G1BeR10b3ahYG7lw2TQCY6aoy4-rFtp_/view?usp=drivesdk" class="Data">🍿Mickey 17</a></li>
-  <li><a href="" class="Data">🍿</a></li>
+  <li><a href="https://drive.google.com/file/d/1IcpH5Q3qiM9Hybjh0SENFjvZYdZ_4feL/view?usp=drivesdk" class="Data">🍿Sinners</a></li>
   <li><a href="" class="Data">🍿</a></li>
   <li><a href="" class="Data">🍿</a></li>
   <li><a href="" class="Data">🍿</a></li>
